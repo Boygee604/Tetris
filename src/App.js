49e8ed00d600013e28c5ca2de07514ca3d08433e
Boygee604 <129235184+Boygee604@users.noreply.github.com>
2023-03-28 17:10:16 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <h2 class="home">HOME</h2>
+        <div class="multiplayer"><h1>MULTIPLAYER</h1>
+        <h4>PLAY WITH ONLINE WITH FRIENDS AND FOES</h4></div>
+        <div class="solo"><h1>SOLO</h1>
+        <h4>CHALLENGE YOURSELF AND TOP THE LEADERBOARDS</h4></div>
+        <div class="tetra"><h1>TETRA CHANNEL</h1>
+        <div></div><h4>LEADERBOARDS, REPLAYS AND MORE</h4></div>
+        <div class="config"><h1>CONFIG</h1>
+        <h4>TWEAK YOUR TETR.IO EXPERIENCE</h4></div>
+        <div  class="about"><h1>ABOUT</h1>
+        <h4>ALL ABOUT TETR.IO</h4></div>
+        <h3 class="welcome">WELCOME TO TETR.IO!</h3>
+    </body>
+    </div>
+  );
+}
+
+export default App;

a994dae7ff0032b972d9465bff0fdd44c3f4a808
Boygee604 <129235184+Boygee604@users.noreply.github.com>
2023-04-04 17:00:26 -0700

    Different pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <body>
-        <h2 class="home">HOME</h2>
-        <div class="multiplayer"><h1>MULTIPLAYER</h1>
-        <h4>PLAY WITH ONLINE WITH FRIENDS AND FOES</h4></div>
-        <div class="solo"><h1>SOLO</h1>
-        <h4>CHALLENGE YOURSELF AND TOP THE LEADERBOARDS</h4></div>
-        <div class="tetra"><h1>TETRA CHANNEL</h1>
-        <div></div><h4>LEADERBOARDS, REPLAYS AND MORE</h4></div>
-        <div class="config"><h1>CONFIG</h1>
-        <h4>TWEAK YOUR TETR.IO EXPERIENCE</h4></div>
-        <div  class="about"><h1>ABOUT</h1>
-        <h4>ALL ABOUT TETR.IO</h4></div>
-        <h3 class="welcome">WELCOME TO TETR.IO!</h3>
-    </body>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/email" element={
+          <p>You can reach me at <a href="mailto:boygee604@gmail.com">boygee604@gmail.com</a>.</p>
+        }/>
+        <Route path="/" element={
+          <div>
+            <body>
+              <h2 class="home">HOME</h2>
+              <a href="/Multiplayer"><div class="multiplayer"><h1>MULTIPLAYER</h1>
+              <h4>PLAY WITH ONLINE WITH FRIENDS AND FOES</h4></div></a>
+              <a href="/Solo"><div class="solo"><h1>SOLO</h1>
+              <h4>CHALLENGE YOURSELF AND TOP THE LEADERBOARDS</h4></div></a>
+              <a href="/Tetra"><div class="tetra"><h1>TETRA CHANNEL</h1>
+              <h4>LEADERBOARDS, REPLAYS AND MORE</h4></div></a>
+              <a href="/Config"><div class="config"><h1>CONFIG</h1>
+              <h4>TWEAK YOUR TETR.IO EXPERIENCE</h4></div></a>
+              <a href="About"><div  class="about"><h1>ABOUT</h1>
+              <h4>ALL ABOUT TETR.IO</h4></div></a>
+              <h3 class="welcome">WELCOME TO TETR.IO!</h3>
+            </body>
+          </div>
+        }></Route>
+        <Route path="/Multiplayer" element={<a href="http://localhost:3000">Home</a>}></Route>
+        <Route path="/Solo" element={<a href="http://localhost:3000">Home</a>}></Route>
+        <Route path="/Tetra" element={<a href="http://localhost:3000">Home</a>}></Route>
+        <Route path="/Config" element={<a href="http://localhost:3000">Home</a>}></Route>
+        <Route path="/About" element={<a href="http://localhost:3000">Home</a>}></Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
